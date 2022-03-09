99b38535e857d0c01fbd6a61532af20dba7e748b
cgeopapa <cgeocodgod@gmail.com>
2022-03-09 13:10:04 +0000

    json export

--- json_export.py
+++ json_export.py
@@ -10,16 +10,22 @@ dirs = os.listdir(loot)
 
 json_obj = {}
 for dir in dirs:
-    if dir not in ["output", "screenshots", "scans", "notes", "output.json", "reports"]:
+    if dir not in ["output", "screenshots", "scans", "notes", "output.json", "reports", "scan.out", "scan.sh"]:
         json_obj[dir] = {}
         for (dirpath, d, files) in os.walk(os.path.join(loot, dir)):
             for file in files:
                 name = os.path.splitext(file)
-                if name[1] != ".xml" or ("websource-http" in name[0]):
-                    f = open(os.path.join(dirpath, file), "r")
-                    contents = f.read().split("\n")
-                    json_obj[dir][name[0]] = contents
+                if (name[1] not in [".xml", ".html", ".old", ".diff"]) and not name[0].startswith("websource-http") and name[0] not in ["targets", "websource-http"] and not name[0].endswith("-unsorted"):
+                    f = open(os.path.join(dirpath, file), "r", encoding="unicode_escape")
+                    contents = f.read().strip().split("\n")
+                    contents = [c.strip() for c in contents]
+                    if len(contents) == 1:
+                        json_obj[dir][name[0]] = contents[0]
+                    else:
+                        json_obj[dir][name[0]] = contents
                 f.close()
 
 f = open(os.path.join(loot, "output.json"), "w")
 f.write(json.dumps(json_obj, indent=4))
+f.close()
+print("Created JSON file at ", os.path.join(loot, "output.json"))
